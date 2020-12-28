eac7c68f5b6cac263b3ca7c8b7220690c3c223d8
florian.weingartshofer <florian.weingartshofer@tuta.io>
2020-12-28 18:50:24 +0100

    Add User-Model

---
+++ server/src/model/user-model.ts
@@ -0,0 +1,20 @@
+import {Document, Schema} from "mongoose";
+import * as mongoose from "mongoose";
+
+export interface IUser extends Document {
+    email: string;
+    name?: string;
+    access_token: string;
+    refresh_token: string;
+    expires_at: Date;
+}
+
+const UserSchema: Schema = new Schema({
+    email: { type: String, required: true},
+    name: { type: String, required: false},
+    access_token: {type: String, required: true},
+    refresh_token: {type: String, required: false},
+    expires_at: {type: Date, required: false}
+})
+
+export default mongoose.model<IUser>("User", UserSchema);
