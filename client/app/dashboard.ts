b419ec0d7542c098c2db0e1ecb6f1414a9c522c5
florian.weingartshofer <florian.weingartshofer@tuta.io>
2020-12-30 18:46:40 +0100

    Update profile view

--- client/app/dashboard.ts
+++ client/app/dashboard.ts
@@ -2,16 +2,15 @@ import {UserService} from "./services/user-service";
 import {UserView} from "../../common/src/view/user-view";
 import {TrackService} from "./services/track-service";
 import {AudioFeatureView} from "../../common/src/view/audio-feature-view";
+import {EndpointService} from "./services/endpoint-service";
 
 function initUserProfileView(user: UserView): void {
-    console.log(user);
     const img = document.getElementById("profile-image") as HTMLImageElement;
     const name = document.getElementById("profile-name");
     const email = document.getElementById("profile-email");
+    img.height = img.width;
     if (user.image) {
-        img.alt = "Profile Picture";
         img.src = user.image;
-        img.height = img.width;
     }
     name.innerText = user.name;
     email.innerText = user.email;
@@ -46,17 +45,14 @@ function initAudioFeature(feature: AudioFeatureView) {
 
 window.onload = () => {
     const params = new URLSearchParams(window.location.search);
-    const code = params.get("id") || localStorage.getItem("id") || null;
+    const code = params.get("id")  || null;
     if (!code) {
         window.location.assign("/app/index.html");
     }
-    localStorage.setItem("id", params.get("id"));
-    const PORT = 3000;
-    const hostname = window.location.hostname;
-    const userService = new UserService(`http://${hostname}:${PORT}`);
+    const userService = new UserService(EndpointService.getEndpoint());
     userService.getUser(params.get("id"))
         .then(initUserProfileView);
 
-    const trackService = new TrackService("http://localhost:3000");
+    const trackService = new TrackService(EndpointService.getEndpoint());
     trackService.getAudioFeature(params.get("id")).then(initAudioFeature);
 }
