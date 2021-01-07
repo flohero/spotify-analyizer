84feac3bc7b4ba2ea1c3d4683a65b200bbcc4bac
awenzelhuemer <43669899+Superandi1234@users.noreply.github.com>
2021-01-07 15:25:00 +0100

    connection string as env variable

--- server/src/config/mongo-config.ts
+++ server/src/config/mongo-config.ts
@@ -3,13 +3,14 @@ import {Logger} from "winston";
 
 export class MongoConfig {
     private readonly opts = {useNewUrlParser: true, useUnifiedTopology: true};
-    private readonly url = "mongodb://root:example@localhost:27017/";
+    readonly connection_string = process.env.MONGODB_CONNECTION_STRING;
+
     constructor(private readonly logger: Logger) {
 
     }
 
     public connect() {
-        mongoose.connect(this.url, this.opts)
+        mongoose.connect(this.connection_string, this.opts)
             .then(() => {
                 this.logger.info("Established connection to MongoDB");
             })
